4fe8e68512f34c6ddbf437234ecb545e754c6372
mrauf2001 <168097042+mrauf2001@users.noreply.github.com>
2024-09-10 12:47:47 +0500

    img

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,21 @@
+import Image from "next/image";
 import Footer from "./components/footer/Footer";
 import Navbar from "./components/navbar/Navbar";
+import bgimg from "./photos/websiteHomePage.jpg";
 export default function Home() {
   return (
     <>
       <div
         className="min-w-full min-h-screen bg-cover bg-center"
-        style={{
-          backgroundImage: "url(/websiteHomePage.jpg)",
-        }}
+        // style={{
+        //   backgroundImage: "url(/websiteHomePage.jpg)",
+        // }}
       >
+        <Image
+          src={bgimg}
+          alt="Background"
+          className="w-full h-screen object-cover absolute right-0 left-0"
+        />
         <Navbar />
         <Footer />
       </div>
